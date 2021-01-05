9346b9c8c9828f7f777fb5081e1b8ca40b35fb12
Christel-WF3 <christelmirassou2@gmail.com>
2021-01-05 16:33:52 +0100

    ma deuxième sauvegarde

--- index.php
+++ index.php
@@ -3,9 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>MON TITRE DE MON SITE</title>
 </head>
 <body>
-    
+    <header>
+        <h1>Titre 1</h1>
+    </header>
+    <main>
+        <section>
+        <h2>Titre 2</h2>
+        </section>
+    </main>
+    <footer>
+        <p>Tous droits réservés</p>
+    </footer>
 </body>
 </html>
